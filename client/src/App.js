f816774327ea548d4c7c30f8582244571407689f
Redas <redass95@outlook.com>
2023-02-21 14:31:55 +0200

    0.01

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>,
-  },
-  {
-    path: "/products/",
-    element: <span>Products</span>,
-  },
-  {
-    path: "/products/:id",
-    element: <span>Product</span>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 function App() {
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center">navbar name</div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
